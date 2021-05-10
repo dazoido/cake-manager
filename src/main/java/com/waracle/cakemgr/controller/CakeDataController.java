a3560159f619d9c819b81f3355735beaa2e74241
Joel Calliste <joel.calliste@ibm.com>
2021-05-10 15:39:34 +0100

    refactor DataControllor to RestController, added json file download endpoint

--- src/main/java/com/waracle/cakemgr/controller/CakeDataController.java
+++ src/main/java/com/waracle/cakemgr/controller/CakeDataController.java
@@ -2,67 +2,92 @@ package com.waracle.cakemgr.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.waracle.cakemgr.data.model.CakeModel;
 import com.waracle.cakemgr.data.repository.CakeRepository;
 import com.waracle.cakemgr.util.CakeDataUtil;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/cakes")
 public class CakeDataController {
 
+    private static final Logger logger = LoggerFactory.getLogger(CakeDataUtil.class);
+
     private CakeRepository cakeRepository;
     private CakeDataUtil cakeDataUtil;
+    private ObjectMapper objectMapper;
 
     @Autowired
-    public CakeDataController(CakeRepository cakeRepository, CakeDataUtil cakeDataUtil) {
+    public CakeDataController(CakeRepository cakeRepository, CakeDataUtil cakeDataUtil, ObjectMapper objectMapper) {
         this.cakeRepository = cakeRepository;
         this.cakeDataUtil = cakeDataUtil;
+        this.objectMapper = objectMapper;
     }
 
     @RequestMapping(method=RequestMethod.GET) 
-    @ResponseBody
-    public String viewAll() {
-        List<CakeModel> cakes = cakeRepository.findAll();
-        
-        if (null != cakes && !cakes.isEmpty()) {
-            return "Found cakes[" + cakes.size() + "]";
-        }
-        return "There are currently no cakes available to view. Please load or add cakes.";
-
+    public List<CakeModel> viewAll() {
+        List<CakeModel> cakes = cakeRepository.findAll();       
+        logger.info("viewAll() Found cakes[" + (null != cakes && !cakes.isEmpty() ? cakes.size() : 0) + "]");      
+        return cakes;
     }
 
-    @GetMapping("/add")
-    @ResponseBody
-    public String add() {
-        CakeModel cake = new CakeModel(
-                            "ChocoCake", 
-                            "A chocolate cake made with hmmm ... chocolate", 
-                            "https://images.immediate.co.uk/production/volatile/sites/30/2020/08/recipe-image-legacy-id-1043451_11-4713959.jpg?quality=90&resize=504,458"
-                            );
+    @RequestMapping(method=RequestMethod.POST)
+    @ResponseStatus(HttpStatus.CREATED) 
+    public void add(@RequestBody @Valid CakeModel cake) throws Exception {
 
-        if (!cakeRepository.existsByTitle(cake.getTitle())) {
-            CakeModel savedCake = cakeRepository.save(cake);
-            return "Added" + savedCake.toString();
+        if (cake != null) {
+            if (cakeRepository.existsByTitle(cake.getTitle())) {
+                throw new Exception("Cake already exits with given title[" + cake.getTitle() + "]");
+            }
+            logger.info("add() saved cake[" + cake.toString() + "]");      
+            cakeRepository.save(cake);
         }
-        return "Cake[" + cake.getTitle() + "] already exists"; 
     }
 
-    @GetMapping("/reset")
-    @ResponseBody
-    public String resetData() {
+    @GetMapping("/resetData")
+    public void resetData() {
         cakeRepository.deleteAll();
         boolean loaded = cakeDataUtil.loadData();
 
         if (loaded) {
-            return "Cake data loaded => " + this.viewAll();
+            logger.info("resetData() Cake data reset");
+        } else {
+            logger.warn("resetData() Unable to reset data");
         }
-        return "Unable to load data";
     }
+
+    @GetMapping("/downloadData")
+    public ResponseEntity<byte[]> downloadData() throws Exception {
+
+		List<CakeModel> cakes = cakeRepository.findAll();
+        
+        // TODO: Should we throw new exception if cakes is null or empty?
+
+		String json = objectMapper.writeValueAsString(cakes);
+		byte[] inputStrBytes = json.getBytes();
+
+		String fileName = "cakes.json";
+		HttpHeaders respHeaders = new HttpHeaders();
+		respHeaders.setContentLength(inputStrBytes.length);
+		respHeaders.setContentType(new MediaType("text", "json"));
+		respHeaders.setCacheControl("must-revalidate, post-check=0, pre-check=0");
+		respHeaders.set(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=" + fileName);
+		return new ResponseEntity<byte[]>(inputStrBytes, respHeaders, HttpStatus.OK);
+    } 
 }
